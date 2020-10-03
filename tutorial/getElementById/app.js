1ebc2d8663dcfe8e4f0f5baf414e59ff8a37a1b2
Sashi Bhushan A Rajput <sam556913@gmail.com>
2020-10-03 21:45:43 +0530

    Adding things that I am learning with tutorial

---
+++ tutorial/getElementById/app.js
@@ -0,0 +1,8 @@
+console.dir(document);
+
+const h1 = document.getElementById('title');
+h1.style.color = 'red';
+
+const btn = document.getElementById('btn');
+btn.style.backgroundColor = "blue";
+btn.style.color = "white";
