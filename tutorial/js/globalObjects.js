cdd4d5f3176061c073c5850a0d48f6556f9d4266
Sashi Bhushan A Rajput <sam556913@gmail.com>
2020-12-21 20:36:09 +0530

    basic things that I am revising about JS

---
+++ tutorial/js/globalObjects.js
@@ -0,0 +1,6 @@
+// *Global Objects
+
+var main = require("./main");
+console.log(__dirname);
+console.log(__filename);
+console.log(main.undercoverIdentity);
---
+++ tutorial/js/main.js
@@ -0,0 +1,17 @@
+console.log("Bonjour");
+
+let DON = "El Locale";
+
+// *Anonymous Function
+var person = function(name){
+  console.log("Yoo "+name)
+}
+person("Raghav");
+
+// *Arrow Function()
+var person = (name) =>{ console.log("Hey "+ name);
+  console.log("Wassup! ?");
+}
+person("Ramlal");
+
+module.exports.undercoverIdentity = DON;
