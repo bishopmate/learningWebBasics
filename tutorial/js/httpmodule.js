088734e4a689997ae575e814fbcccaa221c3fd66
Sashi Bhushan A Rajput <sam556913@gmail.com>
2020-12-22 21:13:52 +0530

    restructuring

--- tutorial/js/httpmodule.js
+++
@@ -1,6 +0,0 @@
-const http = require("http");
-const server = http.createServer(function(req, res){
-  res.writeHead(200, {"content-type" : "text/html"});
-  res.write("<h1>Hey ya, We've been waitin for you</h1>");
-  res.end();
-}).listen(5569, ()=>console.log("Server's up and runnin on our favorite port(5569)"));
