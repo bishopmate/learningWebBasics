196f5f26cf6ba8238d794707525695ac5e33ab85
Sashi Bhushan A Rajput <sam556913@gmail.com>
2020-12-22 16:55:40 +0530

    updating file to include one more example

--- tutorial/js/usingModule.js
+++ tutorial/js/usingModule.js
@@ -1,2 +1,6 @@
-var adder = require("./modules/addition")
+const adder = require("./modules/addition")
+const divider = require("./modules/division");
+
 adder.add(4,5);
+console.log(divider.divide(6,3));
+console.log(divider.divide(7,0));
