324892c15c1e984d7ae29c27602e8795c8bf94e2
Sashi Bhushan A Rajput <sam556913@gmail.com>
2020-12-22 22:26:59 +0530

    Tweaked a little
    Didn't knew that I could use response's write method more than once earlier, but now I know and here's an example

--- learningNodeJS/httpmodule.js
+++ learningNodeJS/httpmodule.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const server = http.createServer(function(req, res){
   res.writeHead(200, {"content-type" : "text/html"});
-  res.write("<h1>Hey ya, We've been waitin for you</h1>");
+  res.write("<h1>Hey ya, We've been waitin for y'all</h1>");
+  res.write("<h1 style = \"font-size:40px;\">So what're you waitin for ?</h1>");
   res.end();
 }).listen(5569, ()=>console.log("Server's up and runnin on our favorite port(5569)"));
