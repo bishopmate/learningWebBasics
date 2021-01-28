0b3394c1ee6ade399ba5d17fb37f81fb799beeb4
Sashi Bhushan A Rajput <sam556913@gmail.com>
2021-01-28 18:58:38 +0530

    added route for Creating post

--- memoriesProject/server/routes/posts.js
+++ memoriesProject/server/routes/posts.js
@@ -1,7 +1,9 @@
 import express from 'express';
 
-import { getPosts } from '../controllers/posts.js'; // named Import
+import { getPosts, createPost } from '../controllers/posts.js'; // named Import
 
 const router = express.Router();
 router.get('/', getPosts);
+router.post('/', createPost);
+
 export default router;
