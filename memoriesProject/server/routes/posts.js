749f59ddf3cca82327ec90ddf29d5d00767a90ce
Sashi Bhushan A Rajput <sam556913@gmail.com>
2021-01-28 16:38:19 +0530

    accidental upload

--- memoriesProject/server/routes/posts.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-import { getPosts } from '../controllers/posts.js'; // named Import
-
-const router = express.Router();
-router.get('/', getPosts);
-export default router;
