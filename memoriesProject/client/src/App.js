547107d976687e9c46a90185adba3232f0037735
Sashi Bhushan A Rajput <sam556913@gmail.com>
2021-01-30 17:13:05 +0530

    making changes for using redux

--- memoriesProject/client/src/App.js
+++ memoriesProject/client/src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import {useDispatch} from 'react-redux';
 
+import { getPosts } from './actions/posts';
 import memories from './images/memories.png';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
@@ -8,6 +10,11 @@ import useStyles from './styles';
  
 const App = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect( () => {
+    dispatch(getPosts);
+  }, [dispatch]);
   return (
     <Container maxWidth = "lg">
       <AppBar className = {classes.appBar} position = "static" color = "inherit">
