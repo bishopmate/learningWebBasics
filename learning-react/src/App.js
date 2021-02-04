9c11b3aae3ad1d3e6368fa2966ff3d9394a3c428
Sashi Bhushan A Rajput <sam556913@gmail.com>
2021-02-04 10:52:49 +0530

    Importing Message and Counter components

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Counter from './Components/Counter';
+import { Counter }from './Components/Counter';
+import { Message } from './Components/Message';
 
 function App() {
+  
   return (
     <div className = "App">
-      <Counter />
+      
     </div>
   );
 }
