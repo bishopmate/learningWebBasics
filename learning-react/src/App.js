729cb5b50a5f8a828f3c38448577a6c7d20daa28
Sashi Bhushan A Rajput <sam556913@gmail.com>
2021-02-05 13:23:23 +0530

    Including Imports for latest Components additions

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import { Counter }from './Components/Counter';
 import { Message } from './Components/Message';
+import { FunctionClick } from './Components/FunctionClick';
+import { ClassClick } from './Components/ClassClick';
+import { EventBind } from './Components/EventBind';
+import { ParentComponent } from './Components/ParentComponent';
 
 function App() {
   
