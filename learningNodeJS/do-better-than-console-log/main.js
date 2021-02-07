ce92836838dfd86064b9bbfaccb584f2ca469342
Sashi Bhushan A Rajput <sam556913@gmail.com>
2021-02-07 13:28:21 +0530

    got introduced a debugger package

---
+++ learningNodeJS/do-better-than-console-log/main.js
@@ -0,0 +1,6 @@
+const debug = require('debug')('on');
+debug("debugging through our new cool debugger XOXO");
+
+const arr = new Array(5569*4569);
+const arr2 = arr.map(item => item);
+debug("debugging finished");
