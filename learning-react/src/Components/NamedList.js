bd3f7d784c98c452498115b78ec89d5b10c858d5
Sashi Bhushan A Rajput <sam556913@gmail.com>
2021-02-14 16:13:12 +0530

    formatting plus adding Component

--- learning-react/src/Components/NamedList.js
+++ learning-react/src/Components/NamedList.js
@@ -1,40 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Person } from "./Person";
 
-function NamedList(){
-  const names = ['Bruce', 'Clark', 'Diana']
+function NamedList() {
+  const names = ["Bruce", "Clark", "Diana"];
   const persons = [
-    { id: 1,
-      name: 'Bruce',
-      age: 30,
-      skill: 'React'
-    },
-    { id: 2,
-      name: 'Clark',
-      age: 25,
-      skill: 'Angular'
-    },
-    { id: 3,
-      name: 'Diana',
-      age: 28,
-      skill: 'Vue'
-    }
-  ]
+    { id: 1, name: "Bruce", age: 30, skill: "React" },
+    { id: 2, name: "Clark", age: 25, skill: "Angular" },
+    { id: 3, name: "Diana", age: 28, skill: "Vue" },
+  ];
 
-  const personList =  names.map(name => { return <div> {name} </div>});
-  const personInfo = persons.map(person => {  
-    return (
-      <div key = {person.id}>
-        I am {person.name} and I am {person.age} years old. I know {person.skill}
-      </div>
-    ) });
-
-  return(
+  const personInfo = persons.map((person) => <Person person={person} />);
+  return (
     <>
       {/* {personList} */}
       {personInfo}
     </>
-  )
-
+  );
 }
 
 export default NamedList;
