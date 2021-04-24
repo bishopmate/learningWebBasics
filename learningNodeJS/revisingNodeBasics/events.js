2604f52711fddf570ebdb24bbf834cc2f72b36da
Sashibhushan rajput <sam556913@gmail.com>
2021-04-24 23:31:35 +0530

    event with multiple listeners

--- learningNodeJS/revisingNodeBasics/events.js
+++ learningNodeJS/revisingNodeBasics/events.js
@@ -4,7 +4,7 @@ const timerEventEmitter = new EventEmitter();
 let currentTime = 0;
 setInterval(()=>{
   currentTime++;
-  timerEventEmitter.emit('secondPassed', currentTime);
+  // timerEventEmitter.emit('secondPassed', currentTime);
 },1000);
 
 timerEventEmitter.once('secondPassed', (currentTime)=>{
@@ -15,3 +15,32 @@ timerEventEmitter.on('secondPassed', (currentTime) =>{
   console.log(`${currentTime} seconds have passed since you came to Heaven`);
 })
 
+const countDown = (countDownTime) => {
+  if(countDownTime <= 2){
+    console.log("Time too short to countdown");
+  }else{
+    const countDownEventEmitter = new EventEmitter();
+    const timer = setInterval(() => {
+      countDownTime--;
+      countDownEventEmitter.emit('count', countDownTime);
+      if(countDownTime == 2){
+        countDownEventEmitter.emit('packup');
+      }
+      if(countDownTime == 0){
+        countDownEventEmitter.emit('end');
+        clearInterval(timer);
+      }      
+    },1000);
+    countDownEventEmitter.on('count', (secondsLeft)=>{
+      console.log(`${secondsLeft} seconds lift till Event Ends`);
+    })
+    countDownEventEmitter.on('packup', ()=>{
+      console.log("Are you ready? !!!!!!!!!!");
+    })
+    countDownEventEmitter.on('end', ()=>{
+      console.log("Yoo, Booooooooom..... The event has ended");
+    })
+  }
+}
+
+countDown(5);
