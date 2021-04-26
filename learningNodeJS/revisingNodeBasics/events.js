839830e9de8b17e09cc5da72654b8dd78f9de6a6
Sashibhushan rajput <sam556913@gmail.com>
2021-04-26 16:16:07 +0530

    error handling and some useful functions

--- learningNodeJS/revisingNodeBasics/events.js
+++ learningNodeJS/revisingNodeBasics/events.js
@@ -16,12 +16,19 @@ timerEventEmitter.on('secondPassed', (currentTime) =>{
 })
 
 const countDown = (countDownTime) => {
+  const countDownEventEmitter = new EventEmitter();
+  
   if(countDownTime <= 2){
-    console.log("Time too short to countdown");
+    countDownEventEmitter.on("error", function rep(err){
+      console.error(err.message);
+    });
+    countDownEventEmitter.emit('error', new Error('Value of the countdown timer can\'t be less than 3'));
   }else{
-    const countDownEventEmitter = new EventEmitter();
     const timer = setInterval(() => {
-      countDownTime--;
+
+    countDownEventEmitter.on('error',function(wrong){
+      console.error(wrong.message);
+    });
       countDownEventEmitter.emit('count', countDownTime);
       if(countDownTime == 2){
         countDownEventEmitter.emit('packup');
@@ -30,6 +37,7 @@ const countDown = (countDownTime) => {
         countDownEventEmitter.emit('end');
         clearInterval(timer);
       }      
+      countDownTime--;
     },1000);
     countDownEventEmitter.on('count', (secondsLeft)=>{
       console.log(`${secondsLeft} seconds left till Event Ends`);
@@ -43,7 +51,7 @@ const countDown = (countDownTime) => {
   }
 }
 
-// countDown(5);
+countDown(2);
 
 class extendedCountDown extends EventEmitter{
   constructor(countDownTime){
@@ -51,31 +59,58 @@ class extendedCountDown extends EventEmitter{
     this.countDownTime = countDownTime;
   }
   startTimer(){
-    const timer = setInterval(() => {
-      this.emit('count', this.countDownTime);
-      if(this.countDownTime == 2){
-        this.emit('packup');
-      }
-      if(this.countDownTime == 0){
-        this.emit('end');
-        clearInterval(timer);
-      }
-      this.countDownTime--;      
-    },1000);
-    this.once('count', (totalSeconds)=>{
-      console.log(`Welcome to Havana, This event is ${totalSeconds} seconds long`);
+     
+    this.on("error", function rep(err){
+      console.error(err.message);
     });
-    this.on('count', (secondsLeft)=>{
-      console.log(`${secondsLeft} seconds left till Event Ends`);
-    })
-    this.on('packup', ()=>{
-      console.log("Are you ready? !!!!!!!!!!");
-    })
-    this.on('end', ()=>{
-      console.log("Yoo, Booooooooom..... The event has ended");
-    }) 
+    if(this.countDownTime <= 2){
+      const err = new Error('Value of the countdown timer cannot be less than 3');
+      this.emit("error", err);
+    }else{
+      const timer = setInterval(() => {
+        this.emit('count', this.countDownTime);
+        if(this.countDownTime == 2){
+          this.emit('packup');
+        }
+        if(this.countDownTime == 0){
+          this.emit('end');
+          clearInterval(timer);
+        }
+        this.countDownTime--;      
+      },1000);
+      this.once('count', (totalSeconds)=>{
+        console.log(`Welcome to Havana, This event is ${totalSeconds} seconds long`);
+      });
+      this.on('count', (secondsLeft)=>{
+        console.log(`${secondsLeft} seconds left till Event Ends`);
+      });
+      this.on('packup', ()=>{
+        console.log("Are you ready? !!!!!!!!!!");
+      });
+      this.on('end', ()=>{
+        console.log("Yoo, Booooooooom..... The event has ended");
+      });
+  }
   }
 }
 
 const sevenSeconds = new extendedCountDown(7);
+console.log(sevenSeconds.eventNames());
 sevenSeconds.startTimer();
+
+const ee = new EventEmitter();
+function ping(){
+  console.log('ping');
+}
+function pong(){
+  console.log('pong');
+}
+ee.on("tt", ping);
+ee.on("tt", pong);
+ee.emit("tt");
+ee.off("tt", ping);// off is an alias for removeListener
+ee.emit("tt");
+ee.on("tt", ping);
+ee.emit("tt");
+ee.removeAllListeners("tt");
+ee.emit("tt");
